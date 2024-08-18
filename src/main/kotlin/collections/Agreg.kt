66278597dc2f7768f42c6035a410024b634daa9f
Linad <merzlikindaniil97@gmail.com>
2024-08-18 13:33:42 +0300

    agreg is done

---
+++ src/main/kotlin/collections/Agreg.kt
@@ -0,0 +1,44 @@
+package collections
+
+fun main() {
+    var array = arrRand(20)
+    println(array.contentDeepToString())
+    println(array.filter { it in 3..9 }.sum())
+
+    val numbers = listOf(1, 2, 3, 4)
+    println(numbers.fold("") { line, element -> line + element.toString() })
+
+    val playerList = listOf(
+        Player("one", (1..10000).random(), (1..1000).random().toDouble()),
+        Player("two", (1..10000).random(), (1..1000).random().toDouble()),
+        Player("three", (1..10000).random(), (1..1000).random().toDouble()),
+        Player("four", (1..10000).random(), (1..1000).random().toDouble()),
+        Player("five", (1..10000).random(), (1..1000).random().toDouble())
+    )
+    playerList.forEach { println(it) }
+    val sumExp = playerList.sumOf { it.exp }
+    val sumMoney = playerList.sumOf { it.money }
+    println("Сумма опыта $sumExp, сумма денег $sumMoney")
+
+    val fruits = listOf("Яблоко", "Абрикос", "Банан", "Виноград", "Вишня", "Кокос", "яблоко")
+    println(fruits)
+    println(fruits.filter { it.length % 2 == 0 }.groupBy { it.first().toString().lowercase() })
+}
+
+fun arrRand(n: Int): Array<Int> {
+    val result = IntArray(n)
+    for (index in 0..<n) {
+        result[index] = (Math.random() * 100).toInt()
+    }
+    return result.toTypedArray()
+}
+
+data class Player(
+    val name: String,
+    val exp: Int,
+    val money: Double
+) {
+    override fun toString(): String {
+        return "Имя $name, опыт $exp, деньги $money."
+    }
+}
